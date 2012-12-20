b3e6b4792a979314f8d9860a4d19d77330ffd553
Colby Fisher <ckf@princeton.edu>
2012-12-20 15:10:11 -0500

    Moved sm_cen_dif to MODULE_SHARED

--- MODULE_LAND.f90
+++ MODULE_LAND.f90
@@ -536,42 +536,6 @@ MODULE MODULE_LAND
 
 ! ====================================================================
 !
-!            subroutine sm_cen_dif
-!
-! ====================================================================
-!
-! Initialize soil moisture for the calculation of the thermodynami!
-! parameters, as a centered difference.
-!
-! ====================================================================
-
-    subroutine sm_cen_dif(iffroz,tkmid,zmid,zrzmax,smtmp,rzsm,tzsm,smold,&
-                            rzsmold,tzsmold)
-
-      implicit none
-      include "help/sm_cen_dif.h"
-
-      iffroz=0
-      if (tkmid.lt.273.15) iffroz=1
-
-      if (zmid.ge.zrzmax) then
-
-         smtmp=0.5*rzsm + 0.5*tzsm
-         smold=0.5*rzsmold + 0.5*tzsmold
-
-      else
-
-         smtmp=rzsm
-         smold=rzsmold
-
-      endif
-
-      return
-
-    end subroutine sm_cen_dif
-
-! ====================================================================
-!
 !                       subroutine states
 !
 ! ====================================================================
