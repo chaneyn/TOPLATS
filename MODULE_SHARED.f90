6d1330db3bc4c862a56d08e30495965c32a80ffb
Colby Fisher <ckf@princeton.edu>
2012-12-20 15:02:18 -0500

    Moved soiltherm to MODULE_SHARED

---
+++ MODULE_SHARED.f90
@@ -0,0 +1,163 @@
+MODULE MODULE_SHARED
+
+contains
+
+
+! ====================================================================
+!
+!             subroutine soiladapt
+!
+! ====================================================================
+!
+! Adapt the thermal parameters for vegetated surfaces.
+!
+! ====================================================================
+
+      subroutine soiladapt(iopgveg,thermc,iopthermc_v,tcbeta,&
+                           xlai,thermc1,heatcap,heatcap1,zero)
+
+      implicit none
+      include "help/soiladapt.h"
+
+! ====================================================================
+! Check the thermal conductivity inputs.
+! ====================================================================
+
+      if ( (thermc.lt.100.d0).and.(thermc.ge.0.d0) )then
+
+         xlai=xlai
+
+      else
+
+         write (*,*) 'Input therm! unrealisti! in SOILADAPT ',thermc
+         write (*,*) iopgveg,thermc,iopthermc_v,tcbeta,&
+                     xlai,thermc1,heatcap,heatcap1,zero
+         stop
+
+      endif
+
+! ====================================================================
+! Modify the thermal parameters for soils under vegetation.
+! ====================================================================
+
+      if (iopgveg.eq.0) then
+
+! --------------------------------------------------------------------&
+! Option 1 : Assume no ground heat flux under vegetation.
+! --------------------------------------------------------------------&
+
+         thermc = zero
+
+      else
+
+! --------------------------------------------------------------------&
+! Option 2 : Assume ground heat flux under vegetation, and an
+!            exponential decay in thermal conducivity of the soil
+!            under vegetation (Choudhury et al., 1987) with LAI.
+! -------------------------------------------------------------------&
+
+         if (iopthermc_v.ne.1) then
+
+            tau = dexp(-tcbeta * xlai)
+            thermc = tau * thermc1
+            heatcap = heatcap1
+
+         else
+
+            thermc = dexp(-tcbeta*xlai)*7.0
+            heatcap = 0.d0
+
+         endif
+
+      endif
+
+! ====================================================================
+! Check the thermal conductivity outputs.
+! ====================================================================
+
+      if ( (thermc.lt.100.d0).and.(thermc.gt.0.d0) )then
+
+         thermc=thermc
+
+      else
+
+         write (*,*) 'Corrected therm! unrealisti! in SOILADAPT ',thermc
+         write (*,*) iopgveg,thermc,iopthermc_v,tcbeta,&
+                     xlai,thermc1,heatcap,heatcap1,zero
+         stop
+
+      endif
+
+      return
+
+      end subroutine soiladapt
+
+
+! ====================================================================
+!
+!                     subroutine soiltherm
+!
+! ====================================================================
+!
+! Calculate the soil thermal parameters.
+!
+! ====================================================================
+
+      subroutine soiltherm(iopthermc,thermc1,thermc2,rzsm,smtmp,&
+       thetar,thetas,psic,bcbeta,tkmid,quartz,ifcoarse,&
+       heatcap1,heatcap2,heatcapold,rocpsoil,row,cph2o,roa,cp,roi,&
+       smold,thermc,heatcap,inc_frozen,rzdthetaudtemp)
+
+      implicit none
+      include "help/soiltherm.h"
+
+! ====================================================================
+! Calculate the termal conductivity.
+! ====================================================================
+
+      if (iopthermc.eq.1) then
+
+! --------------------------------------------------------------------&
+! McCumber-Pielke method
+! --------------------------------------------------------------------&
+
+         thermc1 = calctc_m(rzsm,thetar,thetas,psic,bcbeta)
+         thermc2 = calctc_m(smtmp,thetar,thetas,psic,bcbeta)
+
+      else
+
+! --------------------------------------------------------------------&
+! Johansen's method
+! --------------------------------------------------------------------&
+
+         iffroz=0
+         if (tkmid.lt.273.15) iffroz=1
+         thermc1 = calctc_j(rzsm,thetar,thetas,quartz,iffroz,ifcoarse)
+         thermc2 = calctc_j(smtmp,thetar,thetas,quartz,iffroz,ifcoarse)
+
+      endif
+
+! ====================================================================
+! Calculate the heat capacity.
+! ====================================================================
+
+      heatcap1 = calchc(rzsm,thetas,rocpsoil,row,cph2o,roa,cp,tkmid,roi,&
+                        inc_frozen,rzdthetaudtemp)
+
+! --------------------------------------------------------------------&
+! For the heat capacity of the transmission zone : tzsmpet will
+! lead to overestation of the heat capacity, use average
+! --------------------------------------------------------------------&
+
+      heatcap2 = calchc(smtmp,thetas,rocpsoil,row,cph2o,roa,cp,tkmid,roi,&
+                        inc_frozen,rzdthetaudtemp)
+      heatcapold = calchc(smold,thetas,rocpsoil,row,cph2o,roa,cp,tkmid,roi,&
+                          inc_frozen,rzdthetaudtemp)
+      thermc=thermc1
+      heatcap=heatcap1
+
+      return
+
+      end subroutine soiltherm
+
+END MODULE MODULE_SHARED
