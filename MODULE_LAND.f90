30fb2058107bbffeb2aaed2ffe7baeac005490de
Colby Fisher <ckf@princeton.edu>
2012-12-21 01:08:52 -0500

    Move Moss files to UNUSED folder and moved reset_inf_pars to MODULE_LAND

--- MODULE_LAND.f90
+++ MODULE_LAND.f90
@@ -1139,6 +1139,55 @@ MODULE MODULE_LAND
 
 ! ====================================================================
 !
+!                       subroutine reset_inf_pars
+!
+! ====================================================================
+!
+! Reset cumulative infiltration, initial root zone soil moisture,&
+! sorptivity and dimensionless gravity parameter, cc.
+!
+! ====================================================================
+
+      subroutine reset_inf_pars(cuminf,zero,rzsmst,rzsm,thetas,tolinf,&
+       sorp,two,xk0,psic,thetar,bcgamm,bcbeta,deltrz,cc,one)
+
+      implicit none
+      include "help/reset_inf_pars.h"
+
+! ====================================================================
+! Calculate the root zone soil moisture
+! ====================================================================
+
+      cuminf = zero
+
+      rzsmst = rzsm
+
+      if (rzsmst.ge.thetas)  then
+
+         rzsmst=thetas-tolinf
+
+      endif
+
+! ====================================================================
+! Calculate sorptivity and gravity parameters at the first step
+! of the storm event.
+! ====================================================================
+
+      sorp = (((two*xk0*((thetas-rzsmst)**two)*psic)&
+                   /(thetas-thetar))*((one/(bcgamm+0.5d0*bcbeta-one))+&
+                    ((thetas-thetar)/ (thetas-rzsmst))))**0.5d0
+
+      deltrz = rzsmst-thetar
+      if(deltrz.le.zero) deltrz=zero
+      cc = 0.5d0*(one+ ((deltrz/(thetas-thetar))**(bcgamm/bcbeta)))
+
+      return
+
+      end subroutine reset_inf_pars
+
+
+! ====================================================================
+!
 !                       subroutine ebsres
 !
 ! ====================================================================
