2af5a66ad88c6c8546f1c0d6ae4435a37097b867
Colby Fisher <ckf@princeton.edu>
2012-12-21 01:00:08 -0500

    Moved new_difflx and new_dwnflx to Land Module

--- MODULE_LAND.f90
+++ MODULE_LAND.f90
@@ -2407,6 +2407,350 @@ MODULE MODULE_LAND
 
 ! ====================================================================
 !
+!			subroutine difflx
+!
+! ====================================================================
+!
+! Subroutine difflx calculates the diffusive fluxes out of the surface 
+! and transmission zones (positive down).
+!
+! ====================================================================
+
+      subroutine new_difflx(ikopt,xksrz,xkstz,ff,zrz,ztz,inc_frozen,rzsm,tzsm,&
+       thetas,thetar,bcbeta,psic,difrz,rzsm_u,tzsm_u,diftz)
+
+      implicit none
+      include "help/new_difflx.h"
+
+      data tolsat / 0.001d0 /
+
+! ====================================================================
+! Calculate diffusive flux out of root and transmission zones using 
+! D(theta) relations of Brooks and Corey.
+! ====================================================================
+
+! ====================================================================
+! If root zone not saturated then calculate the drainage.
+! ====================================================================
+
+      if (inc_frozen.eq.0) then
+
+! --------------------------------------------------------------------
+! If frozen soil water is treated as if it were not frozen.
+! --------------------------------------------------------------------
+
+	 if (zrz.gt.zero) then
+
+! ....................................................................
+! The soil profile is not saturated.
+! ....................................................................
+
+	    difrz=diffuse(zrz,ztz,rzsm,(0.5*rzsm+0.5*tzsm),&
+                          thetas,thetar,bcbeta,psic,xksrz,xkstz)
+
+	 else
+
+! ....................................................................
+! The soil profile is saturated.
+! ....................................................................
+
+	    difrz=zero
+
+	 endif
+
+      else
+
+! --------------------------------------------------------------------
+! If frozen soil water is treated as solid soil particles.
+! --------------------------------------------------------------------
+
+         if (zrz.gt.zero) then
+
+! ....................................................................
+! The soil profile is not saturated.
+! ....................................................................
+
+	    difrz=diffuse(zrz,ztz,rzsm_u,(0.5*rzsm_u+0.5*tzsm_u),&
+                          thetas,thetar,bcbeta,psic,xksrz,xkstz)
+
+         else
+! ....................................................................
+! The soil profile is saturated.
+! ....................................................................
+
+	   difrz=zero
+
+	 endif
+
+      endif
+
+! ====================================================================
+! Now repeat calculation for transmission zone.
+! ====================================================================
+
+      if (inc_frozen.eq.0) then
+
+! --------------------------------------------------------------------
+! If frozen soil water is treated as if it were not frozen.
+! --------------------------------------------------------------------
+
+         if (ztz.gt.zero) then
+
+! ....................................................................
+! The soil profile is not saturated.
+!
+! Here we assume diffusion occurs over layer of thickness equal to
+! transmission zone and again account for any nonlinearity in soil
+! moisture profile by averaging gradients.
+! ....................................................................
+
+	    diftz=diffuse(ztz,ztz,tzsm,0.5*tzsm+0.5*thetas,&
+                          thetas,thetar,bcbeta,psic,xkstz,xkstz)
+
+         else
+
+! ....................................................................
+! The soil profile is saturated.
+! ....................................................................
+
+	    diftz=zero
+
+         endif
+
+      else
+
+! --------------------------------------------------------------------
+! If frozen soil water is treated as solid soil particles.
+! --------------------------------------------------------------------
+
+         if (ztz.gt.zero) then
+
+! ....................................................................
+! The soil profile is not saturated.
+! ....................................................................
+
+	    diftz=diffuse(ztz,ztz,tzsm_u,0.5*tzsm_u+0.5*(thetas),&
+                          thetas,thetar,bcbeta,psic,xkstz,xkstz)
+
+         else
+
+! ....................................................................
+! The soil profile is saturated.
+! ....................................................................
+
+            diftz=zero
+
+         endif
+
+      endif
+ 
+      return
+
+      end subroutine new_difflx
+
+! ====================================================================
+!
+!			subroutine diffuse
+!
+! ====================================================================
+!
+! Subroutine diffuse calculates the diffusive flux from layer 1 to layer 2
+! in Richards' Equation using approach of Mahrt and Pan (1984).
+!
+! ====================================================================
+
+      function diffuse(dz1,dz2,theta1,theta2,thetas,thetar,bcbeta,psic,&
+                       xksat1,xksat2)
+
+      implicit none
+      include "help/diffuse.noup.h"
+
+! ====================================================================
+! Calculate diffusivity  using centered approx.
+! ====================================================================
+
+      theta = 0.5d0*(theta1 + theta2)
+
+! ====================================================================
+! Use harmoni! average since flow is perp. to layers.
+! ====================================================================
+
+      xksat = 1/(0.5/xksat1 + 0.5/xksat2)
+
+      F1 = bcbeta * xksat * psic/(thetas-thetar) 
+
+      satrel=(theta-thetar)/(thetas-thetar)
+
+      if (satrel.lt.(0.d0)) satrel=0.d0
+
+      DF = F1 * (satrel) ** (bcbeta + 2.)
+
+! ====================================================================
+! Calculate moisture gradient.
+!
+! dz1 and dz2 are layer thicknesses (not depths) over which diffusion
+! is modeled.
+! ====================================================================
+
+      dz=0.5d0*(dz1+dz2)
+
+      if (dz.gt.1.d-9) then
+
+         grad= (theta1-theta2)/dz
+
+      else
+
+         grad=0.d0
+
+      endif
+
+! ====================================================================
+! Calculate diffusive flux.
+! ====================================================================
+
+      difflx = DF * grad
+
+      diffuse=difflx
+
+      return
+
+      end function diffuse
+
+! ====================================================================
+!
+!			subroutine dwnflx
+!
+! ====================================================================
+!
+! Subroutine dwnflx calculates the downward fluxes out of the root 
+! and transmission zones.
+!
+! ====================================================================
+
+      subroutine new_dwnflx(zrz,ikopt,xksrz,xkstz,rzsm,ff,&
+       inc_frozen,thetar,thetas,&
+       rzsm_u,grz,bcbeta,ztz,gtz,tzsm,tzsm_u)
+
+      implicit none
+      include "help/new_dwnflx.h"
+
+! ====================================================================
+! Calculate downward flux out of root and transmission zones using 
+! ksat(theta) relations of Brooks and Corey.
+! ====================================================================
+
+      if (zrz.eq.zero) then
+
+! ====================================================================
+! If root zone is saturated by the water table then set drainage
+! to zero.
+! ====================================================================
+
+         grz = zero
+
+! ====================================================================
+! If root zone not saturated then calculate the drainage.
+! ====================================================================
+
+      else
+
+! ====================================================================
+! If root zone not saturated then calculate the drainage.
+! ====================================================================
+
+
+! --------------------------------------------------------------------
+! Calcualate relative saturation in the root zone.
+! --------------------------------------------------------------------
+
+         if (inc_frozen.eq.0) then
+
+! ....................................................................
+! Option 1 : Treat frozen soil water as liquid water.
+! ....................................................................
+
+            relsrz = (rzsm-thetar)/(thetas-thetar)
+
+         else
+
+! ....................................................................
+! Option 2 : Treat frozen soil particles as solid soil.
+! ....................................................................
+
+            relsrz = (rzsm_u-thetar)/(thetas-thetar)
+
+         endif
+
+         if (relsrz.le.zero) relsrz=zero
+         if (relsrz.ge.one) relsrz=one
+
+! --------------------------------------------------------------------
+! Calculate downward flux out of root zone.
+! --------------------------------------------------------------------
+
+         grz = xksrz*(relsrz**((two+three*bcbeta)/bcbeta))
+
+      endif
+
+! ====================================================================
+! Now repeat calculation for transmission zone.
+! ====================================================================
+
+      if (ztz.eq.zero) then
+
+! ====================================================================
+! If transmission zone is saturated by the water table then set drainage
+! to zero.
+! ====================================================================
+
+         gtz = zero
+
+      else
+
+! ====================================================================
+! If transmission zone not saturated then calculate the drainage.
+! ====================================================================
+
+
+! --------------------------------------------------------------------
+! Calcualate relative saturation in the transmission zone.
+! --------------------------------------------------------------------
+
+         if (inc_frozen.eq.0) then
+
+! ....................................................................
+! Option 1 : Treat frozen soil water as liquid water.
+! ....................................................................
+
+            relstz=(tzsm-thetar)/(thetas-thetar)
+
+         else
+
+! ....................................................................
+! Option 2 : Treat frozen soil particles as solid soil.
+! ....................................................................
+
+            relstz=(tzsm_u-thetar)/(thetas-thetar)
+
+         endif
+
+         if (relstz.le.zero) relstz=zero
+         if (relstz.ge.one) relstz=one
+
+! --------------------------------------------------------------------
+! Calculate downward flux out of transmission zone.
+! --------------------------------------------------------------------
+
+         gtz = xkstz*(relstz**((two+three*bcbeta)/bcbeta))
+
+      endif
+
+      return
+
+      end subroutine new_dwnflx
+
+! ====================================================================
+!
 !                  subroutine clc_evrz
 !
 ! ====================================================================
--- help/new_difflx.h
+++ help/new_difflx.h
@@ -5,6 +5,5 @@
       real*8 zero,one,two,three,four,five,six
       real*8 tolsat,xksrz,xkstz
 
-      real*8 diffuse
       data zero,one,two,three,four,five,six/0.d0,1.d0,2.d0,&
              3.d0,4.d0,5.d0,6.d0/
