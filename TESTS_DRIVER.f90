40f040e2116e2acb52581d7dd6591a33d88ce545
AmandaSiemann <alsiemann@gmail.com>
2013-01-03 05:22:21 -0500

    added tests for soiladapt,esat,nreb_snow,stabcor,calcra

--- TESTS_DRIVER.f90
+++ TESTS_DRIVER.f90
@@ -84,6 +84,108 @@ contains
     call assert_equals (sm_cen_dif_true,sm_cen_dif_result)
   end subroutine
 
+  subroutine soiladapt_test1()
+    implicit none
+    real*8 :: soiladapt_result,soiladapt_true,thermc
+    call set_unit_name ('soiladapt_test1')
+    call soiladapt(3,2.0d0,1,5.0d0,10.0d0,25.0d0,&
+      3.0d0,4.0d0,0.0d0)
+    soiladapt_result = thermc
+    soiladapt_true = 1.3501249*10**(-21)
+    call assert_equals (soiladapt_true,soiladapt_result)
+  end subroutine
+
+  subroutine soiladapt_test2()
+    implicit none
+    real*8 :: soiladapt_result,soiladapt_true,thermc
+    call set_unit_name ('soiladapt_test2')
+    call soiladapt(10,18.0d0,5,7.0d0,2.0d0,17.0d0,&
+      4.0d0,3.0d0,0.0d0)
+    soiladapt_result = thermc
+    soiladapt_true = 0.00001413598
+    call assert_equals (soiladapt_true,soiladapt_result)
+  end subroutine
+
+  subroutine esat_test1()
+    implicit none
+    real*8 :: esat_result,esat_true
+    call set_unit_name ('esat_test1')
+    esat_result = esat(300.0d0)
+    esat_true = 3535.24205574
+    call assert_equals (esat_true, esat_result)
+  end subroutine
+
+  subroutine esat_test2()
+    implicit none
+    real*8 :: esat_result,esat_true
+    call set_unit_name ('esat_test2')
+    esat_result = esat(260.0d0)
+    esat_true = 221.835008082
+    call assert_equals (esat_true, esat_result)
+  end subroutine
+
+  subroutine nreb_snow_test1()
+    implicit none
+    real*8 :: nreb_snow_result,nreb_snow_true,dzdeep
+    call set_unit_name ('nreb_snow_test1')
+    call nreb_snow(5.0d0,18.0d0,2.0d0,0.0d0,14.0d0,&
+      200.0d0,50.0d0,3.0d0,47.0d0,25.0d0,13.0d0,2.0d0,6.0d0)
+    nreb_snow_result = dzdeep
+    nreb_snow_true = 34.0d0
+    call assert_equals (nreb_snow_true,nreb_snow_result)
+  end subroutine
+
+  subroutine nreb_snow_test2()
+    implicit none
+    real*8 :: nreb_snow_result,nreb_snow_true,gdenom
+    call set_unit_name ('nreb_snow_test2')
+    call nreb_snow(24.0d0,7.0d0,13.0d0,6.0d0,1.0d0,&
+      250.0d0,70.0d0,5.0d0,0.5d0,1.2d0,0.1d0,0.7d0,0.0d0)
+    nreb_snow_result = gdenom
+    nreb_snow_true = 14.516
+    call assert_equals (nreb_snow_true,nreb_snow_result)
+  end subroutine
+
+  subroutine stabcor_test1()
+    implicit none
+    real*8 :: stabcor_result,stabcor_true,uza
+    call set_unit_name ('stabcor_test1')
+    call stabcor(10.0d0,4.0d0,45.0d0,2.5d0,5.0d0,150.0d0,2.0d0,&
+      11.0d0,2.5d0,1.5d0)
+    stabcor_result = uza
+    stabcor_true = -133.621303316
+    call assert_equals (stabcor_true,stabcor_result)
+  end subroutine
+
+  subroutine stabcor_test2()
+    implicit none
+    real*8 :: stabcor_result,stabcor_true,uza
+    call set_unit_name ('stabcor_test2')
+    call stabcor(12.0d0,5.0d0,35.0d0,4.0d0,0.3d0,6.0d0,18.0d0,&
+      37.0d0,4.0d0,2.2d0)
+    stabcor_result = uza
+    stabcor_true = 12.8339112006
+    call assert_equals (stabcor_true,stabcor_result)
+  end subroutine
+
+  subroutine calcra_test1()
+    implicit none
+    real*8 :: calcra_result,calcra_true
+    calcra_result = calcra(0.5d0,5.0d0,8.0d0,4.2d0,10.0d0,&
+      15.0d0,2.0d0)
+    calcra_true = 32.7047922881
+    call assert_equals (calcra_true,calcra_result)
+  end subroutine
+
+  subroutine calcra_test2()
+    implicit none
+    real*8 :: calcra_result,calcra_true
+    calcra_result = calcra(3.0d0,7.0d0,11.0d0,2.2d0,9.0d0,&
+      13.0d0,1.0d0)
+    calcra_true = 0.14378387085
+    call assert_equals (calcra_true,calcra_result)
+  end subroutine
+
   subroutine run_unit_tests()
   
     !Driver to run all the unit tests of the key subroutine in TOPLATS
@@ -100,6 +202,16 @@ contains
     call clcf4temp_test2()
     call sm_cen_dif_test1()
     call sm_cen_dif_test2()
+    call soiladapt_test1()
+    call soiladapt_test2()
+    call esat_test1()
+    call esat_test2()
+    call nreb_snow_test1()
+    call nreb_snow_test2()
+    call stabcor_test1()
+    call stabcor_test2()
+    call calcra_test1()
+    call calcra_test2()
 
     !Summarize and finalize the unit tests
     call fruit_summary
