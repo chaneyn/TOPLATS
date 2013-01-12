994db96942eb2815b68346485d827c3b746bc285
AmandaSiemann <alsiemann@gmail.com>
2013-01-11 23:10:38 -0500

    removed albw-us from atmos last albedo v

--- MODULE_ATMOS.f90
+++ MODULE_ATMOS.f90
@@ -50,7 +50,7 @@ contains
 ! Albedos of the over story, under story,&
 ! and moss layer
 
-       albw_us,&
+       
 
 ! Meteorological data
 
@@ -151,7 +151,7 @@ contains
 !alb_moss = GRID_VEG%alb_moss
 !albd = GRID_VEG%albd
 !albw = GRID_VEG%albw
-albw_us = GRID_VEG%albw_us
+!albw_us = GRID_VEG%albw_us
 !alb_snow = GRID_VARS%alb_snow
 
 !Meteorological data
@@ -534,7 +534,7 @@ iopsmini = GLOBAL%iopsmini
 ! Albedos of the over story, under story,&
 ! and moss layer
 
-       GRID_VEG%alb_moss,GRID_VARS%alb_snow,GRID_VEG%albd,GRID_VEG%albw,albw_us,&
+       GRID_VEG%alb_moss,GRID_VARS%alb_snow,GRID_VEG%albd,GRID_VEG%albw,GRID_VEG%albw_us,&
 
 ! Meteorological data
 
@@ -627,7 +627,7 @@ iopsmini = GLOBAL%iopsmini
       !GRID_VEG%alb_moss = alb_moss
       !GRID_VEG%albd = albd
       !GRID_VEG%albw = albw
-      GRID_VEG%albw_us = albw_us
+      !GRID_VEG%albw_us = albw_us
       !GRID_VARS%alb_snow = alb_snow
 
       !Meteorological Data
