d275530f7bf2d9115446b02f72c751c280d7652a
Nathaniel Chaney <chaneyna@gmail.com>
2013-01-20 18:02:20 -0500

    Changed the directory used in the tests

--- TESTS/Run_Tests.pl
+++ TESTS/Run_Tests.pl
@@ -5,20 +5,23 @@ use strict;
 #LOCAL VARIABLES
 my $FLAG;
 my $i;
-my $GENERAL_FILE = "INPUT/GENERAL_PARAMETER.txt";
-my $NEW_REGIONAL_FILE = "OUTPUT/Regional_Variables.txt";
-my $OLD_REGIONAL_FILE = "OUTPUT_REFERENCE/Regional_Variables.txt";
+my $GENERAL_FILE = "TESTS/INPUT/GENERAL_PARAMETER.txt";
+my $NEW_REGIONAL_FILE = "TESTS/OUTPUT/Regional_Variables.txt";
+my $OLD_REGIONAL_FILE = "TESTS/OUTPUT_REFERENCE/Regional_Variables.txt";
 
 #EXECUTABLES
-my $TOPLATS_NEW = "../TOPLATS_3.0";
-my $UNIT_TESTS = "./TESTS_DRIVER";
-my $CATCHMENT_TESTS = "./CATCHMENT_TESTS_DRIVER";
+my $TOPLATS_NEW = "./TOPLATS_3.0";
+my $UNIT_TESTS = "./TESTS/TESTS_DRIVER";
+my $CATCHMENT_TESTS = "./TESTS/CATCHMENT_TESTS_DRIVER";
+
+chdir("../");
+
+print "Running the unit tests\n";
+print $UNIT_TESTS."\n";
+system("$UNIT_TESTS");
 
 print "Running the model\n";
 system("$TOPLATS_NEW $GENERAL_FILE");
 
 print "Running the catchment tests\n";
 system("$CATCHMENT_TESTS $NEW_REGIONAL_FILE $OLD_REGIONAL_FILE");
-
-print "Running the unit tests\n";
-system("$UNIT_TESTS");
